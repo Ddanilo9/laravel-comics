0af3e2e44a73636e8e0cbb56bad715e9bfbf8734
Ddanilo9 <interiordanilo3@gmail.com>
2022-10-07 19:03:51 +0200

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,64 @@
 <footer>
-    <div>
-        footer
+    <div class="upper-up">
+    <div class="container footer-links">
+      <div class="container-links">
+        <h4>Dc Comics</h4>
+        <ul>
+          <li><a href="">Charachters</a></li>
+          <li><a href="">Comics</a></li>
+          <li><a href="">Movies</a></li>
+          <li><a href="">TV</a></li>
+          <li><a href="">Games</a></li>
+          <li><a href="">Movies</a></li>
+          <li><a href="">News</a></li>
+        </ul>
+        <h4>SHOP</h4>
+        <ul>
+          <li><a href="">Shop DC</a></li>
+          <li><a href="">Shop DC Collectables</a></li>
+        </ul>
+      </div>
+      <div class="container-links">
+        <h4>DC</h4>
+        <ul>
+          <li><a href="">Terms of use</a></li>
+          <li><a href="">Privacy Policies</a></li>
+          <li><a href="">Ad choises</a></li>
+          <li><a href="">Advertising</a></li>
+          <li><a href="">Jobs</a></li>
+          <li><a href="">Subscriptions</a></li>
+          <li><a href="">Talent Workshops</a></li>
+          <li><a href="">CPSC Certificates</a></li>
+          <li><a href="">Ratings</a></li>
+          <li><a href="">Shop helps</a></li>
+          <li><a href="">Contact Us</a></li>
+        </ul>
+      </div>
+      <div class="container-links">
+        <h4>SITES</h4>
+        <ul>
+          <li><a href="">DC</a></li>
+          <li><a href="">MAD Magazine</a></li>
+          <li><a href="">DC kids</a></li>
+          <li><a href="">DC Universe</a></li>
+          <li><a href="">Dc Power Visa</a></li>
+        </ul>
+      </div>
     </div>
-</footer>
+  </div>
+  <div class="bottom-side">
+    <div class="container contact-us">
+      <button>Sign up now</button>
+      <div class="socials">
+        <h4>Follow Us</h4>
+        <ul>
+          <li><a href=""><img src="{{asset('img/footer-facebook.png')}}" alt=""></a></li>
+          <li><a href=""><img src="{{asset('img/footer-twitter.png')}}" alt=""></a></li>
+          <li><a href=""><img src="{{asset('img/footer-youtube.png')}}" alt=""></a></li>
+          <li><a href=""><img src="{{asset('img/footer-pinterest.png')}}" alt=""></a></li>
+          <li><a href=""><img src="{{asset('img/footer-periscope.png')}}" alt=""></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  </footer>
