34352ff7efba963aa87ae02a57d90522eac9537a
Ddanilo9 <interiordanilo3@gmail.com>
2022-10-10 15:49:43 +0200

    add some css card

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -4,10 +4,89 @@
     
 
 @section('content')
-    <div class="container">
-        <img src="{{$card['thumb']}}" alt="">
+    <div class="jumbo">
     </div>
-    <h1>{{$card['series']}}</h1>
-    <p>{{$card['price']}}</p>
+    <div class="box-comic-card">
+        <div class="blue-band">
+            <div class="container">
+                <img src="{{$card['thumb']}}" alt="">
+            </div>
+        
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-70">
+                    <h3>{{$card['title']}}</h3>
+                    <div class="row">
+                        <div class="col-80">
+                            <span>US Price: {{$card['price']}}</span>
+                            <span>Available</span>
+                        </div>
+                        <div class="col-20">
+                            Check Availibility
+                        </div>
+                    </div>
+                    <p>{{$card['description']}}</p>
+                </div>
+                <div class="col-30">
+                    2
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="comics-about">
+        <div class="container">
+            <div class="row">
+                <div class="col-50">
+                    <h3>Talent</h3>
+                    <div class="row">
+                        <div class="col-30">
+                            Art by:
+                        </div>
+                        <div class="col-70">
+                            uno, 2, cndsj, cds
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-30">
+                            Written by:
+                        </div>
+                        <div class="col-70">
+                            cdsac  hudsi hvuish fusahd
+                        </div>
+                    </div>
+                </div>
+                <div class="col-50">
+                    <h3>Specs</h3>
+                    <div class="row">
+                        <div class="col-30">
+                            Series:
+                        </div>
+                        <div class="col-70">
+                            uno, 2, cndsj, cds
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-30">
+                            US Price
+                        </div>
+                        <div class="col-70">
+                            cdsac  hudsi hvuish fusahd
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-30">
+                            On sale date
+                        </div>
+                        <div class="col-70">
+                            cdsac  hudsi hvuish fusahd
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+   
 {{-- @dump($card) --}}
 @endsection
